336ba982c60ae561ccca7ecd08098bd889c6d0af
Alex Boffey <12129558+alexboffey@users.noreply.github.com>
2023-12-12 17:09:28 +0000

    Update .hyper.js

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -12,7 +12,7 @@ module.exports = {
       fontSize: 14,
   
       // font family with optional fallbacks
-      fontFamily: 'Fira Code, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      fontFamily: 'Monaspace Neon Var, Fira Code, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
   
       // default font weight: 'normal' or 'bold'
       fontWeight: 'normal',
